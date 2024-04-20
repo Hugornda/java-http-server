7b933426030337a5bc96bde716875e2e7d1e9017
hugornda <hugornda@gmail.com>
2024-04-20 13:16:30 +0100

    pass 2nd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,31 @@
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    // You can use print statements as follows for debugging, they'll be visible
+    // when running tests.
     System.out.println("Logs from your program will appear here!");
-
-
+    // Uncomment this block to pass the first stage
+    //
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
-
     try {
       serverSocket = new ServerSocket(4221);
       serverSocket.setReuseAddress(true);
       clientSocket = serverSocket.accept(); // Wait for connection from client.
-
-      PrintWriter out  = new PrintWriter(clientSocket.getOutputStream(),true);
+      OutputStream os = clientSocket.getOutputStream();
       String response = "HTTP/1.1 200 OK\r\n\r\n";
+      os.write(response.getBytes());
+      os.close();
+
+      clientSocket.close();
       System.out.println("accepted new connection");
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
+    }
   }
 }
