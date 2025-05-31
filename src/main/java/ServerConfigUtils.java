45c031247b4d1717375d41fe3950ce2eae0da6aa
hugornda <hugornda@gmail.com>
2025-05-31 18:18:39 +0100

    added first file read

--- src/main/java/ServerConfigUtils.java
+++ src/main/java/ServerConfigUtils.java
@@ -6,7 +6,7 @@ public final class ServerConfigUtils {
         for (int i = 0; i < args.length; i++) {
             if (args[i].startsWith("--")) {
                 String key = args[i].substring(2);
-                if ( i+1 < args.length - 1 && !args[i+1].startsWith("--")) {
+                if ( i+1 <= args.length - 1 && !args[i+1].startsWith("--")) {
                     executionParameters.put(key, args[i+1]);
                     ++i;
                 }
